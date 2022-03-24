4d783f5c9d47790d31d82f1a786f0f5a8fc2a07e
Matheus Rayol <matheusrbferreira@gmail.com>
2022-03-23 23:27:51 -0300

    Finalização do Checkpoint I
    Co-authored-by: Maria Bonifácio <Mariaboni@users.noreply.github.com>
    Co-authored-by: Tiago Vale <tpvale@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# FEII-CheckpointI
+# #FEII-CheckpointI
+
+Olá! Este é o repositório do Checkpoint I da disciplina de Front-End II, do curso Certified Tech Developer @ DigitalHouse.
+
+# Equipe
+
+- [Tiago Vieira](https://github.com/tpvale)
+- [Matheus Rayol](https://github.com/matheusrayol)
+- [Maria Bonifácio](https://github.com/mariaboni)
+
+## Tema do Projeto
+
+Para o Checkpoint I foi solicitado que desenvolvêssemos uma interface para criação de cards em qualquer tema, utilizando JavaScript e manipulação do DOM. Optamos por criar uma biblioteca online de jogos, onde o usuário pode registrar jogos, as plataformas onde eles foram lançados, e uma descrição de cada jogo. Como exemplo, a página já inclui seis cards de jogos distintos.
--- assets/scripts/checkpoint.js
+++ assets/scripts/checkpoint.js
@@ -1,7 +1,12 @@
-let capturaElementoPai = document.getElementById("lista-cards");
-let adicionarCard = document.getElementById("btAdicionar");
-let removerCard = document.getElementsByClassName("selecionado");
+// Captura os elementos necessários para o funcionamento do script
+// Lista de cards
+const capturaElementoPai = document.getElementById("lista-cards");
+// Botão para adicionar card
+const adicionarCard = document.getElementById("btAdicionar");
+// Botão para fechar o modal
+const fecharModal = document.getElementById("toggleMyModal")
 
+// Função para inserção de novos cards
 function inserirNovoCard() {
 
     // Variáveis dos campos de cadastro
@@ -26,23 +31,21 @@ function inserirNovoCard() {
     }
 
     // Inserção dos dados do objeto na página com manipulação do DOM
-    const card = `
-        <div class="card text-white bg-dark h-100">
-            <img src="${infoCards.imagem}" class="card-img-top" alt="${infoCards.jogo}">
-            <div class="card-body d-flex flex-column">
-                <h4 class="card-title">${infoCards.jogo}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">${infoCards.plataforma}</h6>
-                <p class="card-text mb-auto mb-2">${infoCards.descricao}</p>
-                <div class="card-footer mt-4">
-                    <a href="#" class="btn btn-primary">Remover item</a>
-                </div>
-            </div>
-        </div>`
+    const card = `<div class="card bg-dark gamegeek-card border-0 h-100">
+        <img class="card-img-top" src="${infoCards.imagem}" alt="${infoCards.jogo}">
+        <div class="card-body d-flex flex-column">
+            <h4 class="card-title font-face-gamegeek text-white">${infoCards.jogo}</h4>
+            <h6 class="card-subtitle text-muted mb-2">${infoCards.plataforma}</h6>
+            <p class="card-text mb-auto text-white">${infoCards.descricao}</p>
+        </div>
+        <div class="card-footer border-0 mb-2">
+        </div>
+    </div>`
 
     // Inserção do card na página
     let novoCard = document.createElement("div");
     novoCard.setAttribute('id', "card");
-    novoCard.setAttribute('class', "col-sm-12 col-md-10 col-lg-8 col-xl-6 col-xxl-4")
+    novoCard.setAttribute('class', "col-sm-12 col-md-7 col-lg-6 col-xl-5 col-xxl-4")
 
     novoCard.innerHTML = card;
     capturaElementoPai.appendChild(novoCard);
@@ -52,10 +55,11 @@ function inserirNovoCard() {
     document.getElementById("inPlataforma").value = "";
     document.getElementById("inImagem").value = "";
     document.getElementById("inDescricao").value = "";
-
 }
 
+// Listener para adição dos cards
 adicionarCard.addEventListener("click", function(evento) {
     evento.preventDefault();
     inserirNovoCard();
+    fecharModal.click();
 });
